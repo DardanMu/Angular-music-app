4d4c0ae9f04c963217c8a7e43e8f183630597bdf
Dardan <dardan@Dardans-MacBook-Pro.local>
2014-03-23 00:36:49 +0000

    updated api request and template

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,7 +10,13 @@ angular.module('angularAppApp')
   })
   .controller('NewCtrl', function ($scope, $http, $resource) {
     $http.defaults.useXDomain = true;
-    var something = $resource('https://ws.spotify.com/search/1/track.json?q=kaizers+orchestra');
+
+    var url = encodeURIComponent('artist:eminem');
+
+    var something = $resource('http://ws.spotify.com/search/1/track.json?q=' + url);
+
+    console.log(something.get());
+
     // $scope.items = ['HTML5 Boilerplate', 'AngularJS', 'Karma'];
     $scope.something = something.get();
   });
