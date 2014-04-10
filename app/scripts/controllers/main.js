e35599060f708ae3f7ad24de8e7b77fc126627a3
Dardan <dardan@Dardans-MacBook-Pro.local>
2014-04-10 01:12:50 +0100

    fixed cors issue, now another issue with template rendering before variable set...

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,26 +8,33 @@ angular.module('angularAppApp')
       'Karma'
     ];
   })
-  .controller('NewCtrl', function ($scope, $http, $resource) {
+  .controller('NewCtrl', function ($scope, $http) {
     $http.defaults.useXDomain = true;
 
-    $scope.master = {};
-    $scope.update = function(user) {
-      $scope.master = angular.copy(user);
-    };
-    var song = $scope.master;
+    // $scope.master = {};
+    // $scope.update = function(user) {
+    //   $scope.master = angular.copy(user);
+    // };
+    // var song = $scope.master;
 
-    console.log('song.name:');
-    console.log(song.name);
+    // console.log('song.name:');
+    // console.log(song.name);
 
     // var url = encodeURIComponent('artist:eminem');
     // var url = encodeURIComponent(song.name);
     var apiKey = '435eb016b95cbac6bfa23a58c7e18e89';
     // var something = $resource('http://ws.spotify.com/search/1/track.json?q=' + url);
-    var something = $resource('http://tinysong.com/b/eminem+monster?format=json&key=' + apiKey);
+    $http.get('http://www.corsproxy.com/tinysong.com/b/eminem+monster?format=json&key=' + apiKey)
+    .success(function(data){
 
-    console.log(something.get());
+      var playlistValue = 'hostname=cowbell.grooveshark.com&songIDs='+ '40220269' +'&bbg=B4D5DA&bth=B4D5DA&pfg=B4D5DA&lfg=B4D5DA&bt=813B45&pbg=813B45&pfgh=813B45&si=813B45&lbg=813B45&lfgh=813B45&sb=813B45&bfg=B1BABF&pbgh=B1BABF&lbgh=B1BABF&sbh=B1BABF&p=0';
 
-    // $scope.items = ['HTML5 Boilerplate', 'AngularJS', 'Karma'];
-    $scope.something = something.get();
+      $scope.plalistValue = playlistValue;
+      $scope.data = data;
+      
+    });
+
+    $scope.templates = [ { name: 'template1.html', url: 'views/template1.html'} ];
+    $scope.template = $scope.templates[0];
+
   });
