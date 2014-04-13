c2ef6c90f227200991fb725252e2061161d92eaa
Dardan <dardan@Dardans-MacBook-Pro.local>
2014-04-13 22:31:51 +0100

    fixed that issue finally

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -21,12 +21,24 @@ angular.module('angularAppApp')
       var playlistValue = 'hostname=cowbell.grooveshark.com&songIDs='+ '40220269' +'&bbg=B4D5DA&bth=B4D5DA&pfg=B4D5DA&lfg=B4D5DA&bt=813B45&pbg=813B45&pfgh=813B45&si=813B45&lbg=813B45&lfgh=813B45&sb=813B45&bfg=B1BABF&pbgh=B1BABF&lbgh=B1BABF&sbh=B1BABF&p=0';
       $scope.playlistValue = playlistValue;
       $scope.data = data;
+
+      // angular.element('#test').remove();
       
     });
   })
   .directive('ngPlaylistwidget', function(){
     return {
-        templateUrl: 'views/templates/flash-widget.html'
+        // templateUrl: 'views/templates/flash-widget.html'
+        link: function(scope, element, attrs) {
+          attrs.$observe('data', function(value) {
+            if (value) {
+              element.html(
+                '<object width="450" height="250"><param name="movie" value="http://grooveshark.com/widget.swf"><param name="wmode" value="window"><param name="allowScriptAccess" value="always"><param name="flashvars" value="'+value+'"></object>');
+            } else {
+              element.html('<div></div>'); // We have to put something into the DOM
+            }
+          });
+        },
       };
   });
   
