83145eb581ce1a81e654faf60df3893809d35f6a
Dardan <dardan@Dardans-MacBook-Pro.local>
2014-04-13 01:49:50 +0100

    small typo fix

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -29,7 +29,7 @@ angular.module('angularAppApp')
 
       var playlistValue = 'hostname=cowbell.grooveshark.com&songIDs='+ '40220269' +'&bbg=B4D5DA&bth=B4D5DA&pfg=B4D5DA&lfg=B4D5DA&bt=813B45&pbg=813B45&pfgh=813B45&si=813B45&lbg=813B45&lfgh=813B45&sb=813B45&bfg=B1BABF&pbgh=B1BABF&lbgh=B1BABF&sbh=B1BABF&p=0';
 
-      $scope.plalistValue = playlistValue;
+      $scope.playlistValue = playlistValue;
       $scope.data = data;
       
     });
