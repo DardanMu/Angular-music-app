064499da647b47d9d9f895023f56c2814f339dd7
Dardan <dardan@Dardans-MacBook-Pro.local>
2014-04-26 21:40:37 +0100

    added a simple form to search for artists

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,20 +11,26 @@ angular.module('angularAppApp')
   .controller('NewCtrl', function ($scope, $http) {
     $http.defaults.useXDomain = true;
 
-    // var url = encodeURIComponent('artist:eminem');
-    // var url = encodeURIComponent(song.name);
+    $scope.artist2 = {};
     var apiKey = '435eb016b95cbac6bfa23a58c7e18e89';
-    // var something = $resource('http://ws.spotify.com/search/1/track.json?q=' + url);
-    $http.get('http://www.corsproxy.com/tinysong.com/b/eminem+monster?format=json&key=' + apiKey)
-    .success(function(data){
+ 
+    $scope.update = function(artist) {
+      // var artist = angular.copy(artist);
+      // console.log(artist);
+      $scope.artist2 = angular.copy(artist);
+      var urlQuery = encodeURIComponent(artist.name);
 
-      var playlistValue = 'hostname=cowbell.grooveshark.com&songIDs='+ '40220269' +'&bbg=B4D5DA&bth=B4D5DA&pfg=B4D5DA&lfg=B4D5DA&bt=813B45&pbg=813B45&pfgh=813B45&si=813B45&lbg=813B45&lfgh=813B45&sb=813B45&bfg=B1BABF&pbgh=B1BABF&lbgh=B1BABF&sbh=B1BABF&p=0';
-      $scope.playlistValue = playlistValue;
-      $scope.data = data;
+      $http.get('http://www.corsproxy.com/tinysong.com/b/'+urlQuery+'?format=json&key=' + apiKey)
+      .success(function(data){
+        var playlistValue = 'hostname=cowbell.grooveshark.com&songIDs='+ data.SongID +'&bbg=B4D5DA&bth=B4D5DA&pfg=B4D5DA&lfg=B4D5DA&bt=813B45&pbg=813B45&pfgh=813B45&si=813B45&lbg=813B45&lfgh=813B45&sb=813B45&bfg=B1BABF&pbgh=B1BABF&lbgh=B1BABF&sbh=B1BABF&p=0';
+        console.log(playlistValue);
+        $scope.playlistValue = playlistValue;
+        $scope.data = data;
+        // angular.element('#test').remove();
+      });
+    };
 
-      // angular.element('#test').remove();
-      
-    });
+
   })
   .directive('ngPlaylistwidget', function(){
     return {
@@ -33,9 +39,13 @@ angular.module('angularAppApp')
           attrs.$observe('data', function(value) {
             if (value) {
               element.html(
-                '<object width="450" height="250"><param name="movie" value="http://grooveshark.com/widget.swf"><param name="wmode" value="window"><param name="allowScriptAccess" value="always"><param name="flashvars" value="'+value+'"></object>');
+                '<object width="450" height="250">'+
+                  '<param name="movie" value="http://grooveshark.com/widget.swf">'+
+                  '<param name="wmode" value="window"><param name="allowScriptAccess" value="always">'+
+                  '<param name="flashvars" value="'+value+'">'+
+                '</object>');
             } else {
-              element.html('<div></div>'); // We have to put something into the DOM
+              element.html('<div></div>');
             }
           });
         },
