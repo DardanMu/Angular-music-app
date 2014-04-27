a650ddd40c1ebbe3bb062e0eefab91bfaec21881
Dardan <dardan@Dardans-MacBook-Pro.local>
2014-04-28 00:04:36 +0100

    updated css for flash player

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -56,7 +56,7 @@ angular.module('angularMusicApp').
           attrs.$observe('data', function(value) {
             if (value) {
               element.html(
-                '<object width="90%" height="380">'+
+                '<object height="380">'+
                   '<param name="movie" value="http://grooveshark.com/widget.swf">'+
                   '<param name="wmode" value="window"><param name="allowScriptAccess" value="always">'+
                   '<param name="flashvars" value="hostname=cowbell.grooveshark.com&songIDs='+value+'&bbg=B4D5DA&bth=B4D5DA&pfg=B4D5DA&lfg=B4D5DA&bt=813B45&pbg=813B45&pfgh=813B45&si=813B45&lbg=813B45&lfgh=813B45&sb=813B45&bfg=B1BABF&pbgh=B1BABF&lbgh=B1BABF&sbh=B1BABF&p=0">'+
