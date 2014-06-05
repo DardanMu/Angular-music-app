e2e8961ff2a97c508f28740dcfd9e47818c0de12
Dardan <dardan@Dardans-MacBook-Pro.local>
2014-06-05 21:56:36 +0100

    used .then

--- app/scripts/controllers/songCtrl.js
+++ app/scripts/controllers/songCtrl.js
@@ -6,15 +6,15 @@ app.controller('SongCtrl', function ($scope, lastFmFactory, groovesharkFactory, 
       var artistQuery = encodeURIComponent(artist.name);
 
       //last fm api
-      lastFmFactory.getArtistData(artistQuery).success(function(data){
-
+      lastFmFactory.getArtistData(artistQuery).then(function(response){
+        var data = response.data;
         $scope.artistData = data.artist;
         $scope.artistBio = $sce.trustAsHtml(data.artist.bio.summary);
         artistQuery = data.artist.name;
 
         //grooveshark api
-        groovesharkFactory.getSongs(artistQuery).success(function(songs){
-
+        groovesharkFactory.getSongs(artistQuery).then(function(response){
+          var songs = response.data;
           var playlistValue = '';
           songs.forEach(function(song) {
             playlistValue = playlistValue + song.SongID +',';
