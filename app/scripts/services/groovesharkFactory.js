f0adb42b507d7c55f758bc3565b54921fe330ff2
Dardan <Dardan.mu@gmail.com>
2014-06-21 19:08:47 +0100

    removed unused properties in api objects

--- app/scripts/services/groovesharkFactory.js
+++ app/scripts/services/groovesharkFactory.js
@@ -4,14 +4,12 @@ app.factory('groovesharkFactory', function($http){
     $http.defaults.useXDomain = true;
 
     var groovesharkApi = {
-      'apiUrl'    : 'http://www.corsproxy.com/tinysong.com/s/',
-      'urlParams' : '?format=json&limit=10&key=',
-      'apiKey'    : '435eb016b95cbac6bfa23a58c7e18e89'
+      'apiUrl'    : 'http://music-app-server.local:3000/api/songs?artist='
     };
     
     var factory = {};
     factory.getSongs = function(artistName){
-      return $http.get(groovesharkApi.apiUrl+artistName+groovesharkApi.urlParams+groovesharkApi.apiKey);
+      return $http.get(groovesharkApi.apiUrl+artistName);
     };
     return factory;
   });
--- app/scripts/services/lastFmFactory.js
+++ app/scripts/services/lastFmFactory.js
@@ -4,14 +4,12 @@ app.factory('lastFmFactory', function($http){
     $http.defaults.useXDomain = true;
 
     var lastFmApi = {
-      'apiUrl'    : 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=',
-      'urlParams' : '&autocorrect=1&format=json&api_key=',
-      'apiKey'    : 'b58473f5006d02f1809cfec98c17011d'
+      'apiUrl'    : 'http://music-app-server.local:3000/api/artist?name='
     };
 
     var factory = {};
     factory.getArtistData = function(artistName){
-      return $http.get(lastFmApi.apiUrl+artistName+lastFmApi.urlParams+lastFmApi.apiKey);
+      return $http.get(lastFmApi.apiUrl+artistName);
     };
     return factory;
   });
