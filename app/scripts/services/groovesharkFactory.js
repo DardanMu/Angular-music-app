796d85261206fd28025f84ad95fc13be29b3309c
Dardan <Dardan.mu@gmail.com>
2014-10-05 18:44:51 +0100

    updated the endpoints

--- app/scripts/services/groovesharkFactory.js
+++ app/scripts/services/groovesharkFactory.js
@@ -4,12 +4,12 @@ app.factory('groovesharkFactory', function($http){
     $http.defaults.useXDomain = true;
 
     var groovesharkApi = {
-      'apiUrl'    : 'http://music-app-server.local:3000/api/songs?artist='
+      'songsByArtist'    : 'http://music-app-server.local:3000/api/v1.0/songsByArtist?artist='
     };
-    
+
     var factory = {};
     factory.getSongs = function(artistName){
-      return $http.get(groovesharkApi.apiUrl+artistName);
+      return $http.get(groovesharkApi.songsByArtist+artistName);
     };
     return factory;
   });
--- app/scripts/services/lastFmFactory.js
+++ app/scripts/services/lastFmFactory.js
@@ -4,12 +4,37 @@ app.factory('lastFmFactory', function($http){
     $http.defaults.useXDomain = true;
 
     var lastFmApi = {
-      'apiUrl'    : 'http://music-app-server.local:3000/api/artist?name='
+      'artistInfo'          : 'http://music-app-server.local:3000/api/v1.0/artist?name=',
+      'artistEvents'        : 'http://music-app-server.local:3000/api/v1.0/eventsByArtist',
+      'eventsByLocation'    : 'http://music-app-server.local:3000/api/v1.0/eventsByLocation',
+      'topArtists'          : 'http://music-app-server.local:3000/api/v1.0/topArtistsByLocation',
+      'hypedArtists'        : 'http://music-app-server.local:3000/api/v1.0/hypedArtists'
     };
 
     var factory = {};
+
     factory.getArtistData = function(artistName){
-      return $http.get(lastFmApi.apiUrl+artistName);
+      return $http.get(lastFmApi.artistInfo+artistName);
     };
+
+    factory.getArtistEventData = function(artistName, location){
+
+    };
+
+    factory.getEventsByLocation = function(location){
+
+    };
+
+    factory.getTopArtists = function(location){
+
+    };
+
+    factory.getHypedArtists = function(location){
+
+    };
+
+
+
+
     return factory;
   });
