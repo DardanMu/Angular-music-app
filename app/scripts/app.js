cc443fb73c6eaa815d83122f195c574b0ce78486
Dardan <Dardan.mu@gmail.com>
2014-10-05 20:09:31 +0100

    now using angular ui router, and created a homepage controller

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,20 +4,39 @@ angular.module('angularMusicApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'ui.router'
 ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'SongCtrl',
-        reloadOnSearch: false
-      })
-      // .when('/about', {
-      //   templateUrl: 'views/about.html',
-      //   controller: 'MainCtrl'
-      // })
-      .otherwise({
-        redirectTo: '/'
-      });
+  // .config(function ($routeProvider) {
+  //   $routeProvider
+  //     .when('/', {
+  //       templateUrl: 'views/main.html',
+  //       controller: 'SongCtrl',
+  //       reloadOnSearch: false
+  //     })
+  //     // .when('/about', {
+  //     //   templateUrl: 'views/about.html',
+  //     //   controller: 'MainCtrl'
+  //     // })
+  //     .otherwise({
+  //       redirectTo: '/'
+  //     });
+  // });
+
+
+  .config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise("/");
+
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "views/home.html",
+      controller: 'HomeCtrl'
+    })
+    .state('look-up-artist', {
+      url: "/look-up-artist",
+      templateUrl: "views/main.html",
+      controller: 'SongCtrl'
+    })
   });
---
+++ app/scripts/controllers/homeCtrl.js
@@ -0,0 +1,7 @@
+'use strict';
+var app = angular.module('angularMusicApp');
+app.controller('HomeCtrl', function ($scope, lastFmFactory, groovesharkFactory, $state) {
+
+  console.log('homepage');
+
+});
