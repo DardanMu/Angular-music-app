e2dc243174e1d70e7e0b02309b9d453fa5027c93
Dardan <Dardan.mu@gmail.com>
2014-10-24 00:51:11 +0100

    calling the top artists http url from home controller

--- app/scripts/controllers/homeCtrl.js
+++ app/scripts/controllers/homeCtrl.js
@@ -10,7 +10,7 @@ app.controller('HomeCtrl', function ($scope, apiDataFactory, $state, usersGeoloc
 
   geoLocationPromise.then(function(data){
       $scope.location = data;
-      console.log(data);
+      // console.log(data);
   });
 
   $scope.look_up_artist = function(artistObj)
@@ -18,11 +18,24 @@ app.controller('HomeCtrl', function ($scope, apiDataFactory, $state, usersGeoloc
       $state.go('look-up-artist', {artist: artistObj.name});
   }
 
+    //hyped artists
     apiDataFactory.getHypedArtists()
         .then(function(hypedArtists){
             $scope.hypedArtists = hypedArtists.data.artists.artist;
-            console.log(hypedArtists.data);
+            // console.log(hypedArtists.data);
         });
+
+
+    //top artsits
+    geoLocationPromise.then(function(location){
+
+        apiDataFactory.getTopArtists(location)
+            .then(function(topArtists){
+                $scope.topArtists = topArtists.data.topartists.artist;
+                // console.log(topArtists.data);
+            });
+
+    });
 
 
 
