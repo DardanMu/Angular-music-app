448396f6a59b46fea93062cb0dc8f807cb3c0556
Dardan <Dardan.mu@gmail.com>
2014-11-11 16:07:26 +0000

    added some styling to the events page

--- app/scripts/services/usersGeolocationFactory.js
+++ app/scripts/services/usersGeolocationFactory.js
@@ -10,10 +10,8 @@ app.factory('usersGeolocationFactory', function($q, geolocation){
         var deferred = $q.defer();
 
         if (location) {
-            console.log('already got it lol');
             deferred.resolve(location);
         }else{
-            console.log('searching...');
             geolocation.getLocation().then(function(data){
                 location = {lat:data.coords.latitude, long:data.coords.longitude};
                 deferred.resolve(location);
