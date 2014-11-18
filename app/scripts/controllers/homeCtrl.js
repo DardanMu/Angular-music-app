92505bee6a1ae0a953481eef808b3ec88e35ff74
Dardan <Dardan.mu@gmail.com>
2014-11-18 00:50:28 +0000

    created a new filter range to loop over a specific number in an ngrepeat loop

--- app/scripts/controllers/homeCtrl.js
+++ app/scripts/controllers/homeCtrl.js
@@ -42,9 +42,7 @@ app.controller('HomeCtrl', function ($filter, $scope, apiDataFactory, $state, us
 
     });
 
-    $scope.dataset_test = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     $scope.currentPage = 1;
-
     $scope.updateEventPage = function(pageNumber)
     {
         $scope.events = null;
