b320d942bfa68eba5d863bed70eb5576b657dc62
Dardan <Dardan.mu@gmail.com>
2014-11-20 01:17:15 +0000

    created a truncate filter to prevent long event titles from messing with the layout

---
+++ app/scripts/filters/truncate.js
@@ -0,0 +1,29 @@
+'use strict';
+
+app.filter('truncate', function() {
+    return function(text, length, end) {
+
+        if (text.length <= length || text.length - end.length <= length) {
+            return text;
+        }
+        else {
+            return String(text).substring(0, length-end.length) + end;
+        }
+
+    };
+});
+
+/**
+ * Usage
+ *
+ * var myText = "This is an example.";
+ *
+ * {{myText|Truncate}}
+ * {{myText|Truncate:5}}
+ * {{myText|Truncate:25:" ->"}}
+ * Output
+ * "This is..."
+ * "Th..."
+ * "This is an e ->"
+ *
+ */
